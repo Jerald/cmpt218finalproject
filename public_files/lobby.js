da2434361df389fb116c89e7b6f9e3c89391d943
Oscar <osmithsi@sfu.ca>
2018-04-08 18:41:33 -0700

    Added additional lobby explanation

--- public_files/lobby.js
+++ public_files/lobby.js
@@ -1,10 +1,11 @@
 // See lobby.html for primer on the design for this system.
 
+// For proper cleanup, we cannot define any global variables. Use closure instead. If we need global variables, also set them as undefined as per below.
 // Dynamic page protocol will use the following function for cleanup:
 
 function changePage (data)
 {
-    // Clear every function *except* main socket.io loop
+    // Clear every function *except* main socket.io loop.
     // For example, if we have a function called foo() which we've set, we clear it with the following:
     // foo = undefined
     // This in fact will clear the code from the browser's memory.
@@ -21,6 +22,7 @@ function changePage (data)
 
 // This is an example of the ajax to call to load the game page. Copy the body to where it gets used. It'll probably be triggered by the start button in some way.
 // The url and some other values may need to be changed.
+
 function ajaxLoadGame ()
 {
     $.ajax({
@@ -29,3 +31,10 @@ function ajaxLoadGame ()
         success: changePage
     });
 }
+
+// To use the socketio connection, we abuse global scope. We define the socket variable in global scope, so once it's defined we can access it anywhere.
+// This persists after we reload the page, assuming we don't delete it like the other variables. Example:
+var socketio; // This is the connection variable, do NOT delete it.
+// This socketio object still needs to be constructed in some ready function in this script.
+// Construction example: socketio = io();
+
